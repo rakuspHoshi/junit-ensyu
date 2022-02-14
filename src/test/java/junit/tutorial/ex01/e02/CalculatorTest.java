9be90a5bb5898625e125052ffc4b767b91c0f595
rakuspHoshi <ryuto.hoshi@rakus-partners.co.jp>
2022-02-14 10:54:53 +0900

    ex01-e02終了

--- src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
+++ src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
@@ -3,12 +3,24 @@ package junit.tutorial.ex01.e02;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 class CalculatorTest {
+	
+//	@Autowired
+//	public Calculator calculator;
+	
+	Calculator calculator = new Calculator();
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void divideTest() {
+		System.out.println("スタート");
+		try {
+			calculator.divide(2, 0);
+		} catch (IllegalArgumentException e) {
+			assertTrue(e.getMessage().matches("divide by zero."));
+			System.out.println("例外を取得");
+		}
 	}
 
 }
