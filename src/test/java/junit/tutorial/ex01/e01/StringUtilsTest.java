dad47b9f6904e88b31c0ffab98526c696f16c522
rakuspHoshi <ryuto.hoshi@rakus-partners.co.jp>
2022-02-14 10:32:53 +0900

    ex01-e01終了

--- src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
+++ src/test/java/junit/tutorial/ex01/e01/StringUtilsTest.java
@@ -2,6 +2,28 @@ package junit.tutorial.ex01.e01;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class StringUtilsTest {
+	
+	@Test
+	public void testAaa() {
+		assertEquals("aaa", StringUtils.toSnakeCase("aaa"), "aaaじゃない");
+		System.out.println("aaaテスト終了");
+	}
+	
+	@Test
+	public void testHello() {
+		assertEquals("hello_world", StringUtils.toSnakeCase("HelloWorld"), "hello_worldじゃない");
+		System.out.println("HelloWorldテスト終了");
+	}
+	
+	@Test
+	public void testPractice() {
+		assertEquals("practice_junit", StringUtils.toSnakeCase("practiceJunit"), "practice_junitじゃない");
+		System.out.println("practiceJunitテスト終了");
+	}
+	
+	
 
 }
--- src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
+++ src/test/java/junit/tutorial/ex01/e02/CalculatorTest.java
@@ -1,10 +1,10 @@
-package junit.tutorial.ex01.e01;
+package junit.tutorial.ex01.e02;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class StringUtilsTest2 {
+class CalculatorTest {
 
 	@Test
 	void test() {
